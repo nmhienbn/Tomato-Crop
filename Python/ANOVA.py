Correct Tukey's LSD formula. Generalize for unequal observations.

Python/ANOVA.py
@@ -52,9 +52,9 @@ def perform_anova_summary(
         # Extract Turkey's HSD values
         q_crit = studentized_range.ppf(1 - alpha, n_groups, dfw)
 
-        lsd = q_crit * np.sqrt(2 * msw / n_per_group)
+        lsd = q_crit * np.sqrt(msw / n_per_group)
 
-        charGroup = TukeyHSD.getLabels(groups_mean, lsd)
+        charGroup = TukeyHSD.getLabels(groups_mean, groups_size, q_crit, msw)
         lsd = round(lsd, 2)
     else:
         lsd = "ns"
@@ -79,11 +79,13 @@ def perform_anova(data, alpha=0.05):
         q_crit = studentized_range.ppf(
             1 - alpha, n_groups, n_groups * n_per_group - n_groups
         )
+        # print(q_crit, mse, n_groups)
 
         # Turkey's HSD characteristic group
         groups_mean = [np.mean(group) for group in data]
-        lsd = q_crit * np.sqrt(2 * mse / n_per_group)
-        charGroup = TukeyHSD.getLabels(groups_mean, lsd)
+        groups_size = [len(group) for group in data]
+        lsd = q_crit * np.sqrt(mse / n_per_group)
+        charGroup = TukeyHSD.getLabels(groups_mean, groups_size, q_crit, mse)
 
         lsd = round(lsd, 2)
     else:

Python/TukeyHSD.py
@@ -16,23 +16,26 @@ def find_maximal_cliques(edges):
     return sorted_cliques
 
 
-def getLabels(groups_mean, lsd):
+def getLabels(groups_mean: list, groups_size: list, q_crit: float, msw: float):
     # Perform Tukey's HSD test
 
     significant_pairs = []
     is_all = True
     for i in range(len(groups_mean)):
         for j in range(i + 1, len(groups_mean)):
+            lsd = q_crit * np.sqrt(msw / 2 * (1 / groups_size[i] + 1 / groups_size[j]))
+            # print(lsd)
             if np.abs(groups_mean[i] - groups_mean[j]) < lsd:
                 significant_pairs.append((i, j))
             else:
                 is_all = False
 
-    # Find maximal cliques
-    maximal_cliques = find_maximal_cliques(significant_pairs)
     charGroup = [""] * len(groups_mean)
     if is_all:
         return charGroup
+
+    # Find maximal cliques
+    maximal_cliques = find_maximal_cliques(significant_pairs)
     current_letter = "a"
     # Set the character group
     for clique in maximal_cliques: